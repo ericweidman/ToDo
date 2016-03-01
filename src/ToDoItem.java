2412787392ebda1eb288d7c8a74d0da71a8c70cc
ericweidman <ericweidman@gmail.com>
2016-03-01 10:35:59 -0500

    Added DB

--- src/ToDoItem.java
+++ src/ToDoItem.java
@@ -2,10 +2,12 @@
  * Created by ericweidman on 2/8/16.
  */
 public class ToDoItem {
+    public int id;
     String text;
     boolean isDone;
 
-    public ToDoItem(String text, boolean isDone) {
+    public ToDoItem(int id, String text, boolean isDone) {
+        this.id = id;
         this.text = text;
         this.isDone = isDone;
     }
